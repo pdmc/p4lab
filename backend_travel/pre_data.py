1da2cab8dccaaf8775088fef23b92f43be5e8fbe
wangjipeng <zhanghui@pk4yo.com>
2018-07-17 09:55:36 +0800

    add pre_data.py

--- backend_travel/pre_data.py
+++ backend_travel/pre_data.py
@@ -3,7 +3,8 @@ import pymysql
 
 def get_name():
     name_lis = []
-    db = pymysql.connect("192.168.8.105","weibospider","wangwang","lab")
+    #db = pymysql.connect("192.168.8.105","weibospider","wangjipeng","p4lab")
+    db = pymysql.connect("101.236.16.93","pk4yo","f23kKF34Q3r9","p4lab",charset='utf8')
     cursor = db.cursor()
     sql = 'select name from national_museum'
     cursor.execute(sql)
